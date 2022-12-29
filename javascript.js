712ff48db923534e70983c3528323075e5ef8c86
Cat1tac <waynepowell235@gmail.com>
2022-12-29 11:50:37 -0500

    Made all children fit in parent equally

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ const sketch = document.querySelector('#container');
 boxes();
 
 function boxes() {
-    for (let i = 0; i < 256; i++){
+    for (let i = 0; i < 100; i++){
         var box = document.createElement('div');
         box.classList.add('block');
 
@@ -25,8 +25,6 @@ function boxes() {
 function newGrid() {
     const size = prompt("Enter size of one side of the grid");
     const square = size * size;
-    sketch.style.width = (size * 2 * 22) + "px";
-    console.log(sketch.style.width);
     const vanish = document.querySelectorAll('.block');
     vanish.forEach(block => {
         block.remove();
@@ -35,9 +33,11 @@ function newGrid() {
     for (let i = 0; i < square; i++){
         var box = document.createElement('div');
         box.classList.add('block');
-
+        box.style.width = ((640 / size) - 2) + "px";
+        box.style.height = ((640 / size) - 2) + "px";
         sketch.appendChild(box);
     }
+    
     const hover = document.querySelectorAll('.block');
     hover.forEach((div) => {
         div.addEventListener('mouseover', function (e) {
