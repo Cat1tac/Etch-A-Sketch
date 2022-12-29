e337ebae3f6686b154025148d624cf0a4e02271f
Cat1tac <waynepowell235@gmail.com>
2022-12-29 12:18:30 -0500

    implemented a slider

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
 const sketch = document.querySelector('#container');
+const slider = document.querySelector('#myRange');
+const sliderOutput = document.querySelector('#sliderValue');
+sliderOutput.innerHTML = slider.value;
+
 boxes();
 
 function boxes() {
@@ -8,6 +12,7 @@ function boxes() {
 
         sketch.appendChild(box);
     }
+    //Lets user draw over blocks
     const hover = document.querySelectorAll('.block');
     hover.forEach((div) => {
         div.addEventListener('mouseover', function (e) {
@@ -15,21 +20,23 @@ function boxes() {
         });
     }); 
 
-    const grid = document.querySelector('button');
-    grid.addEventListener("click", () => {
+    slider.addEventListener("change", () => {
+        sliderOutput.innerHTML = slider.value;
         newGrid();
     });
 
 }
 
 function newGrid() {
-    const size = prompt("Enter size of one side of the grid");
+    const size = slider.value;
     const square = size * size;
     const vanish = document.querySelectorAll('.block');
+    //Removes all blocks
     vanish.forEach(block => {
         block.remove();
     })
     
+    //Replaces all blocks with new ones
     for (let i = 0; i < square; i++){
         var box = document.createElement('div');
         box.classList.add('block');
